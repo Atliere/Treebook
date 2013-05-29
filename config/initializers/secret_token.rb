aa01242f5ff480afdc2d9403f3c8d222317115be
Mitchell Richardson <richards@cs.dal.ca>
2013-05-29 10:15:58 -0300

    test comit broken

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '23fdff4e5b728edcd9c69c77b218c52b5589c3b0f8a65e3ddbaebcd0ed95820e2a7e7d412fe0651970366a53d046f4eff529b81652c3aa149deb28e3e2f04441'
